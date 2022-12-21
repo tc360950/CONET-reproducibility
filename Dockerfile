aee13809f545c6ccc4d14e7e0a10461c461efd40
neuro <t.cakala@neurosys.com>
2022-12-21 07:56:23 +0100

    Added CBS+MergeLevels

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,11 @@ USER root
 RUN apt-get update && apt-get install -y python3-pip
 RUN pip install jupyterlab
 
+RUN R -e "install.packages('BiocManager',dependencies=TRUE, repos='http://cran.rstudio.com/')"
+RUN R -e "BiocManager::install('DNAcopy')"
+RUN R -e "BiocManager::install('aCGH')"
+RUN R -e "install.packages('optparse',dependencies=TRUE, repos='http://cran.rstudio.com/')"
+
 WORKDIR /code
 
 COPY . .
@@ -34,3 +39,4 @@ RUN apt-get install libgraphviz-dev
 
 COPY script.py ./
 ENTRYPOINT ["python3", "script.py"]
+#ENTRYPOINT ["sleep", "infinity"]
--- script.py
+++ script.py
@@ -1,9 +1,12 @@
+import random
+
 import numpy as np
-import sys 
+import sys
+
 sys.path.append("./python/")
 from numpy import genfromtxt
-import conet 
-import networkx as nx 
+import conet
+import networkx as nx
 import conet.src.data_converter.data_converter as dc
 import conet.src.conet as c
 import conet.src.conet_parameters as cp
@@ -13,31 +16,35 @@ from conet.src.per_bin_model.node_label import *
 from conet.src.per_bin_model.tree_generator import *
 from conet.src.per_bin_model.counts_generator import *
 from pydantic import BaseModel
-from typing import List 
-import tempfile 
-from pathlib import Path 
-import math 
-import argparse 
-import json 
+from typing import List
+import tempfile
+from pathlib import Path
+import math
+import argparse
+import json
+import subprocess
 
-class SimulationConfig(BaseModel):  
+
+class SimulationConfig(BaseModel):
     tree_size: int = 20
-    cells: int = 200# add output folder for stats                                                                              
-    bins: int  = 1500
-    high_noise: bool = False  
+    cells: int = 200  # add output folder for stats
+    bins: int = 1500
+    high_noise: bool = False
     n: int = 5
     conet_binary_dir: str = "/code"
+    known_breakpoints: bool = True
+
 
 class Statistics(BaseModel):
-    tree_size: int 
-    edge_precision: float 
-    edge_sensitivity: float 
-    node_precision: float 
-    node_sensitivity: float 
-    RMSE: float 
-    FP: float 
-    FN: float 
-    SymmetricDifference: int 
+    tree_size: int
+    edge_precision: float
+    edge_sensitivity: float
+    node_precision: float
+    node_sensitivity: float
+    RMSE: float
+    FP: float
+    FN: float
+    SymmetricDifference: int
 
 
 def load_config(path) -> SimulationConfig:
@@ -51,29 +58,29 @@ def generate_model(tree_size, loci, cells, id_, high_noise: bool = False, out_di
         out_dir += "/"
     if high_noise:
         cn_s = \
-            CNSampler({0:0.020240121,1: 0.203724532,3: 0.050340118,4: 0.038828672, 2: 0.686866557},
-                    {0:0.449,1: 0.116,2: 1.41 * 0.187,3: 0.114,4: 0.279,5: 0.0957,6: 0.4833,7: 0.2760,8: 6.15780,9: 4.72105270})
-        cn_s.NOISE_PROB=0.1
+            CNSampler({0: 0.020240121, 1: 0.203724532, 3: 0.050340118, 4: 0.038828672, 2: 0.686866557},
+                      {0: 0.449, 1: 0.116, 2: 1.41 * 0.187, 3: 0.114, 4: 0.279, 5: 0.0957, 6: 0.4833, 7: 0.2760,
+                       8: 6.15780, 9: 4.72105270})
+        cn_s.NOISE_PROB = 0.01
     else:
         cn_s = \
-            CNSampler({0:0.020240121,1: 0.203724532,3: 0.050340118,4: 0.038828672, 2: 0.686866557},
-                    {0:0.449,1: 0.116,2: 0.187,3: 0.114,4: 0.279,5: 0.0957,6: 0.4833,7: 0.2760,8: 6.15780,9: 4.72105270})
-        cn_s.NOISE_PROB=0.01
+            CNSampler({0: 0.020240121, 1: 0.203724532, 3: 0.050340118, 4: 0.038828672, 2: 0.686866557},
+                      {0: 0.449, 1: 0.116, 2: 0.187, 3: 0.114, 4: 0.279, 5: 0.0957, 6: 0.4833, 7: 0.2760, 8: 6.15780,
+                       9: 4.72105270})
+        cn_s.NOISE_PROB = 0.01
 
     t_gen = TreeGenerator(cn_s)
     d_gen = CountsGenerator(cn_s)
-        
-        
+
     tree, trunk = t_gen.generate_random_tree(loci, tree_size)
     data = d_gen.generate_data(loci, tree, cells, trunk)
-    
-    
+
     np.savetxt(out_dir + "diffs_" + id_, data[3], delimiter=";", fmt='%.6f')
     np.savetxt(out_dir + "counts_" + id_, data[0], delimiter=";", fmt='%.6f')
     np.savetxt(out_dir + "corrected_counts_" + id_, data[2], delimiter=";", fmt='%.6f')
     np.savetxt(out_dir + "attachment_" + id_, data[1], delimiter=";", fmt='%.0f')
     nx.write_edgelist(tree, out_dir + "tree_" + id_)
-    
+
     return data, tree
 
 
@@ -81,27 +88,42 @@ def save_counts_in_CONET_format(path: str, counts: np.ndarray, indices: List[int
     no_cells = counts.shape[0]
     no_loci = counts.shape[1]
     counts = np.transpose(counts)
-    
+
     # Add columns required by CONET
     add = np.zeros([no_loci, 5], dtype=np.float64)
     add.fill(1)
-    add[:,1] = range(0, no_loci)
-    add[:,2] = range(1, no_loci+1)
-    add[:,4] = 0
+    add[:, 1] = range(0, no_loci)
+    add[:, 2] = range(1, no_loci + 1)
+    add[:, 4] = 0
     add[indices, 4] = 1
     full_counts = np.hstack([add, counts])
-    
-    # Add first row with cell names 
+
+    # Add first row with cell names
     names = np.zeros([1, 5 + no_cells], dtype=np.float64)
     names[0, 5:] = range(0, no_cells)
     full_counts = np.vstack([names, full_counts])
-    
-    
+
     np.savetxt(path, full_counts, delimiter=";")
+
 
 parser = argparse.ArgumentParser(description='Run CONET')
 parser.add_argument('--config', type=str, required=True)
 parser.add_argument('--out', type=str, required=True)
+
+
+def precision(A, B) -> float:
+    C = A.intersection(B)
+    if len(A) > 0:
+        return len(C) / len(A)
+    return 0.0
+
+
+def sensitivity(A, B) -> float:
+    C = A.intersection(B)
+    if len(B) > 0:
+        return len(C) / len(B)
+    return 0.0
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -111,47 +133,51 @@ if __name__ == "__main__":
     statistics: List[Statistics] = []
     if not dirpath.endswith("/"):
         dirpath += "/"
-    def precision(A, B) -> float:
-        C = A.intersection(B)
-        if len(A) > 0:
-            return len(C) / len(A)
-        return 0.0 
-
-    def sensitivity(A, B) -> float:
-        C = A.intersection(B)
-        if len(B) > 0:
-            return len(C) / len(B)
-        return 0.0 
 
     for i in range(conf.n):
         MODEL_ID = f"model_{i}"
         print(f"Generating model num {i}")
-        data, tree = generate_model(conf.tree_size, conf.bins, conf.cells, MODEL_ID, high_noise=conf.high_noise, out_dir=dirpath)
+        data, tree = generate_model(conf.tree_size, conf.bins, conf.cells, MODEL_ID, high_noise=conf.high_noise,
+                                    out_dir=dirpath)
         print(f"Model generated")
-        # Extract real breakpoints indices
-        real_ind = list(map(lambda x : x.start, list(tree.nodes)))
-        real_ind.extend(list(map(lambda x : x.end, list(tree.nodes))))
+        corr_reads = genfromtxt(dirpath + 'corrected_counts_' + MODEL_ID, delimiter=';')
+
+        real_ind = list(map(lambda x: x.start, list(tree.nodes)))
+        real_ind.extend(list(map(lambda x: x.end, list(tree.nodes))))
         real_ind = list(set(real_ind))
         real_ind.sort()
+        if conf.known_breakpoints:
+            # Extract real breakpoints indices
+            candidates = real_ind
+            save_counts_in_CONET_format(dirpath + "counts_synthetic", corr_reads, candidates)
+        else:
+            save_counts_in_CONET_format(dirpath + "counts_synthetic", corr_reads, [])
+            subprocess.run(["Rscript", "CBS_MergeLevels.R", "--mincells=5",
+                            f"--output={dirpath}cbs_out", f"--dataset={dirpath}counts_synthetic"])
+            X = np.loadtxt(dirpath + "cbs_out", delimiter=",")
+            candidates = []
+            for i in range(X.shape[0]):
+                if X[i, 4] == 1.0:
+                    candidates.append(i)
+            save_counts_in_CONET_format(dirpath + "counts_synthetic", corr_reads, candidates)
+            print(f"Found {len(candidates)} breakpoint candidates...")
 
-        # Convert model data to CONET format 
-        corr_reads = genfromtxt(dirpath + 'corrected_counts_' + MODEL_ID, delimiter=';')
-        save_counts_in_CONET_format(dirpath + "counts_synthetic", corr_reads, real_ind)
-        data_converter = dc.DataConverter(dirpath +"counts_synthetic", 
-                                        delimiter= ';', 
-                                        default_bin_length = 1, 
-                                        event_length_normalizer = corr_reads.shape[1], # number of loci
-                                        add_chromosome_ends = False,
-                                        neutral_cn = 2.0)
+        # Convert model data to CONET format
+        data_converter = dc.DataConverter(dirpath + "counts_synthetic",
+                                          delimiter=';',
+                                          default_bin_length=1,
+                                          event_length_normalizer=corr_reads.shape[1],  # number of loci
+                                          add_chromosome_ends=False,
+                                          neutral_cn=2.0)
         data_converter.create_CoNET_input_files(dirpath, add_chr_ends_to_indices=False)
 
-            # Perform CONET inference
+        # Perform CONET inference
         conet = c.CONET(str(Path(conf.conet_binary_dir) / Path("CONET")))
-        params = cp.CONETParameters(tree_structure_prior_k1 = 0.01, 
-                                        data_dir = dirpath, counts_penalty_s1=100000, counts_penalty_s2=100000, 
-                                        param_inf_iters=500000, seed = 2167, mixture_size=2, pt_inf_iters=1000000,
+        params = cp.CONETParameters(tree_structure_prior_k1=0.01,
+                                    data_dir=dirpath, counts_penalty_s1=100000, counts_penalty_s2=100000,
+                                    param_inf_iters=500000, seed=random.randint(0, 1000), mixture_size=2, pt_inf_iters=1000000,
                                     use_event_lengths_in_attachment=False,
-                                    event_length_penalty_k0 = 1)
+                                    event_length_penalty_k0=1)
         conet.infer_tree(params)
         print(f"CONET inference finished on model {i}")
         result = ir.InferenceResult(conf.conet_binary_dir, corr_reads.T)
@@ -166,19 +192,39 @@ if __name__ == "__main__":
         real_nodes = set((n.start, n.end) for n in tree.nodes)
         real_edges = set(((e[0].start, e[0].end), (e[1].start, e[1].end)) for e in tree.edges)
 
+        FP = 0.0
+        FN = 0.0
+        SymmetricDifference = 0.0
+        real_bp_count = 0.0
+        inferred_bp_count = 0.0
+
+        for cell in range(0, inferred_cn.shape[1]):
+            for bin in range(1, inferred_cn.shape[0]):
+                if inferred_cn[bin, cell] != inferred_cn[bin - 1, cell]:
+                    inferred_bp_count += 1.0
+
+                if real_cn.T[bin, cell] != real_cn.T[bin -1, cell]:
+                    real_bp_count += 1.0
+
+                if inferred_cn[bin, cell] != inferred_cn[bin -1, cell] and real_cn.T[bin, cell] == real_cn.T[bin -1, cell]:
+                    SymmetricDifference += 1.0
+                    FP += 1.0
+                elif inferred_cn[bin, cell] == inferred_cn[bin -1, cell] and real_cn.T[bin, cell] != real_cn.T[bin -1, cell]:
+                    SymmetricDifference += 1.0
+                    FN += 1.0
+
         statistics.append(
             Statistics(
                 tree_size=len(inferred_nodes),
-                RMSE = math.sqrt(np.mean((inferred_cn - real_cn.T) ** 2)),
+                RMSE=math.sqrt(np.mean((inferred_cn - real_cn.T) ** 2)),
                 edge_precision=precision(inferred_edges, real_edges),
                 edge_sensitivity=sensitivity(inferred_edges, real_edges),
                 node_precision=precision(inferred_nodes, real_nodes),
-                node_sensitivity=precision(inferred_nodes, real_nodes),
-                FP = np.sum(inferred_brkp - real_brkp == 1) / np.sum(inferred_brkp) if np.sum(inferred_brkp) > 0 else 0.0,
-                FN = np.sum(inferred_brkp - real_brkp == -1) / np.sum(real_brkp) if np.sum(real_brkp) > 0 else 0.0,
-                SymmetricDifference=np.sum(np.abs(inferred_brkp - real_brkp)) / float(conf.cells)
+                node_sensitivity=sensitivity(inferred_nodes, real_nodes),
+                FP=FP / inferred_bp_count if inferred_bp_count > 0.0 else 0.0 ,
+                FN=FN / real_bp_count if real_bp_count > 0.0 else 0.0,
+                SymmetricDifference=SymmetricDifference / float(conf.cells)
             ).dict()
         )
     with open(args.out, "w") as f:
         f.write(json.dumps(statistics, indent=4))
-        
